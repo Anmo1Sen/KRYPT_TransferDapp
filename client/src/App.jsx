33e0fc1a6aee33e3e1081a826968abc1efebdf46
Anmo1Sen <anmolsen73@example.com>
2022-10-07 17:54:47 +0530

    Final

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar,Welcome,Footer,Services,Transactions } from "./components"
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+    </div>
+  )
+}
---
+++ client/src/utils/constants.js
@@ -0,0 +1,6 @@
+
+
+import abi from "./Transactions.json";
+
+export const contractAddress = "0xE06CaE92436a6E8Da675734dFF633795958DD76d";
+export const contractABI = abi.abi;
